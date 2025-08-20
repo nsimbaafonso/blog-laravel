2ef43b7b57bc02318cf30a443e2ad8e128120c3e
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-08-20 21:24:04 +0100

    Script de posts slider adicionado na página de busca

--- resources/views/site/busca.blade.php
+++ resources/views/site/busca.blade.php
@@ -143,4 +143,5 @@
 
 @push('scripts')
     <script type="text/javascript" src="{{ asset('js/swiper-bundle.min.js') }}?<?= time() ?>"></script>
+    <script type="text/javascript" src="js/posts-slider.js"></script>
 @endpush
