368124293f4cff08b67f34e1101b23cc46ecf568
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-08-22 10:36:36 +0100

    Estrutura para exibir mensagem de erro adicionado

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,6 +5,11 @@
         <form action="{{ route('login') }}" method="POST" class="login-form" autocomplete="off">
             @csrf
             <h3>PAINEL ADMINISTRATIVO</h3>
+            <div class="alert success">
+                <i class="fas fa-check-circle"></i>
+                <span class="message">Operação realizada com sucesso!</span>
+                <button class="close-btn-alert"><i class="fas fa-times"></i></button>
+            </div>
             <input type="email" name="email" placeholder="exemplo@gmail.com" class="box" required>
             <input type="password" name="password" placeholder="Sua senha" class="box" required>
             <div class="remember">
