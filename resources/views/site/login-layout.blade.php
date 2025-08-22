2cc6c12f4d7c1985a25036068d6eaee380d87486
Nsimba Afonso <nsimbaafonso0@gmail.com>
2025-08-22 09:48:05 +0100

    Layout do login feito

---
+++ resources/views/site/login-layout.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-ao">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    <link href="{{ asset('fonts/fontawesome-6.5.2/css/fontawesome.css') }}?<?= time() ?>" rel="stylesheet">
+    <link href="{{ asset('fonts/fontawesome-6.5.2/css/brands.css') }}?<?= time() ?>" rel="stylesheet">
+    <link href="{{ asset('fonts/fontawesome-6.5.2/css/solid.css') }}?<?= time() ?>" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}?<?= time() ?>">
+    <link rel="stylesheet" href="{{ asset('css/login.css') }}?<?= time() ?>">
+</head>
+<body>
+    <!--login-->
+    @yield('conteudo')
+    <!--login end-->
+</body>
+</html>
+
